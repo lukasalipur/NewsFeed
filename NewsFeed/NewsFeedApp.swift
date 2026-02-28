ec8a259d1d3343649fb916660118dc3f5ee9f60c
Luka <lukasalipur.dev@gmail.com>
2026-02-28 15:23:36 +0100

    Initial Commit

---
+++ NewsFeed/NewsFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsFeedApp.swift
+//  NewsFeed
+//
+//  Created by Luka Šalipur on 28. 2. 2026..
+//
+
+import SwiftUI
+
+@main
+struct NewsFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
